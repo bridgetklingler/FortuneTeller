41ff12e02b0c3ecc080f825ffb1e8349f4260931
Bridget Klingler <bridget.klingler@gmail.com>
2019-05-16 09:49:07 -0400

    cleaned \n lines

--- Program.cs
+++ Program.cs
@@ -8,27 +8,24 @@ namespace FortuneTeller
         {
             Console.WriteLine("What is your first name?");
             string name = Console.ReadLine();
-            Console.WriteLine($"Hello {name}");
-            Console.ReadLine();
+            Console.WriteLine($"\nHello {name}");
                
             Console.WriteLine("What is your last name?");
             string lastName = Console.ReadLine();
-            Console.WriteLine($"Thank you {name} {lastName}");
-            Console.ReadLine();
+            Console.WriteLine($"\nThank you {name} {lastName}");
 
             Console.WriteLine("How old are you?");
             string age = Console.ReadLine();
             int ageValue = Convert.ToInt32(age);
 
-            Console.WriteLine($"{age} huh? Interesting...");
+            Console.WriteLine($"\n{age} huh? Interesting...");
             
             
-            Console.WriteLine($"{name}, please enter your birth month as a number between 1-12");
+            Console.WriteLine($"\n{name}, please enter your birth month as a number between 1-12");
             string month = Console.ReadLine();
             Console.WriteLine("Getting closer to your fortune...");
-            Console.ReadLine();
 
-            Console.WriteLine("What is your favorite RYOGBIV color? If unsure type \"Help\".");
+            Console.WriteLine("\nWhat is your favorite RYOGBIV color? If unsure type \"Help\".");
             string colorResponse = Console.ReadLine();
             if (colorResponse.ToLower().Equals("help")){
                 Console.WriteLine("ROYGBIV Colors are: Red, Yellow, Orange, Blue, Green, Indigo, and Violet.");
@@ -51,6 +48,27 @@ namespace FortuneTeller
             else if (ageValue < 72 || ageValue >= 45) { Console.WriteLine("You're almost ready to retire!"); };
             Console.ReadLine();
 
+            if (colorResponse.ToLower().Equals("red"))
+            {
+                Console.WriteLine("You'll retire in Rome, Italy!");
+            }
+            else if (colorResponse.ToLower().Equals("orange"))
+            {
+                Console.WriteLine("You'll retire in Moscow, Russia.");
+            }
+            else if (colorResponse.ToLower().Equals("yellow"))
+            {
+                Console.WriteLine("You'll retire in a cave in Arizona.");
+            }
+            else if (colorResponse.ToLower().Equals("green"))
+            {
+                Console.WriteLine("You'll retire on a personal island in the Carribean! (Make sure to bring the sunscreen!)");
+            }
+            else if (colorResponse.ToLower().Equals("blue"))
+            {
+                Console.WriteLine("You're going to retire in a mansion in Greece!");
+            }
+            Console.ReadLine();
         }
     }
 }

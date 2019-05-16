7da92eda973110478b88dba75a4b6b9e85d6b5c1
Bridget Klingler <bridget.klingler@gmail.com>
2019-05-16 11:00:42 -0400

    changed nubmer values

--- Program.cs
+++ Program.cs
@@ -9,65 +9,66 @@ namespace FortuneTeller
             Console.WriteLine("What is your first name?");
             string name = Console.ReadLine();
             Console.WriteLine($"\nHello {name}");
-               
+
             Console.WriteLine("What is your last name?");
             string lastName = Console.ReadLine();
             Console.WriteLine($"\nThank you {name} {lastName}");
 
             Console.WriteLine("How old are you?");
             string age = Console.ReadLine();
-            int ageValue = Convert.ToInt32(age);
+            int ageValue = Int32.Parse(age);
+            int retireAge = 0;
 
             Console.WriteLine($"\n{age} huh? Interesting...");
-            
-            
+
+
             Console.WriteLine($"\n{name}, please enter your birth month as a number between 1-12");
             string month = Console.ReadLine();
-            Console.WriteLine("Getting closer to your fortune...");
+            Console.WriteLine("\nGetting closer to your fortune...");
 
-            Console.WriteLine("\nWhat is your favorite RYOGBIV color? If unsure type \"Help\".");
+            Console.WriteLine("What is your favorite RYOGBIV color? If unsure type \"Help\".");
             string colorResponse = Console.ReadLine();
-            if (colorResponse.ToLower().Equals("help")){
+            if (colorResponse.ToLower().Equals("help")) {
                 Console.WriteLine("ROYGBIV Colors are: Red, Yellow, Orange, Blue, Green, Indigo, and Violet.");
             }
-            Console.ReadLine();
 
-            Console.WriteLine("How many siblings do you have?");
+            Console.WriteLine("\nHow many siblings do you have?");
             string siblingValue = Console.ReadLine();
-            Console.WriteLine("Thank you for answering my questions. Press enter for your fortune!");
- 
+            Console.WriteLine("\nThank you for answering my questions. Press enter for your fortune!");
+
             if (ageValue >= 72)
             {
-                Console.WriteLine("You're an old fart!");
+                Console.WriteLine("Maserati");
             }
             else if (ageValue >= 0 || ageValue < 25)
             {
-                Console.WriteLine("You have your whole life ahead of you!");
+                Console.WriteLine("Unicycle");
             }
-            else if (ageValue < 45 || ageValue >= 25) { Console.WriteLine("You're so young!"); }
-            else if (ageValue < 72 || ageValue >= 45) { Console.WriteLine("You're almost ready to retire!"); };
-            Console.ReadLine();
+            else if (ageValue < 45 || ageValue >= 25) { Console.WriteLine("Private Jet Plane"); }
+            else if (ageValue < 72 || ageValue >= 45) { Console.WriteLine("Zip Line"); };
 
             if (colorResponse.ToLower().Equals("red"))
             {
-                Console.WriteLine("You'll retire in Rome, Italy!");
+                Console.WriteLine("in Rome, Italy!");
             }
             else if (colorResponse.ToLower().Equals("orange"))
             {
-                Console.WriteLine("You'll retire in Moscow, Russia.");
+                Console.WriteLine("in Moscow, Russia.");
             }
             else if (colorResponse.ToLower().Equals("yellow"))
             {
-                Console.WriteLine("You'll retire in a cave in Arizona.");
+                Console.WriteLine("in a cave in Arizona.");
             }
             else if (colorResponse.ToLower().Equals("green"))
             {
-                Console.WriteLine("You'll retire on a personal island in the Carribean! (Make sure to bring the sunscreen!)");
+                Console.WriteLine("on a personal island in the Carribean! (Make sure to bring the sunscreen!)");
             }
             else if (colorResponse.ToLower().Equals("blue"))
             {
-                Console.WriteLine("You're going to retire in a mansion in Greece!");
+                Console.WriteLine("in a mansion in Greece!");
             }
+
+            Console.WriteLine($"{name} {lastName}, You will retire {colorResponse}, driving a {ageValue}. \nThank you for playing!");
             Console.ReadLine();
         }
     }
